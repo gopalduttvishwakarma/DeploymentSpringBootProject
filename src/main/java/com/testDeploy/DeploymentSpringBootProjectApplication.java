fa63b4a32731579094f705a7eb440969d8b7dd72
Gopal Dutt Vishwakarma <gopalduttvishwakarma@gmail.com>
2024-02-03 21:36:16 +0530

    Project for deploye

---
+++ src/main/java/com/testDeploy/DeploymentSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.testDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentSpringBootProjectApplication.class, args);
+	}
+
+}
